448660a7e07f177da9220e1e84228b3fcab8bff9
Gerald Ezenagu <egerald344@gmail.com>
2023-10-25 02:25:11 +0100

    server-t

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Server = require('http');
+const createServer = Server.createServer;
+const mongoConnect = require('./database/mongoConnect').mongoConnect;
+
+
+
+const app = express();
+const httpServer = createServer(app);
+const cors = require("cors");
+const PORT = 7000;
+require("dotenv").config();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use('/', (req, res) => {
+    res.send('Server is running')
+})
+
+mongoConnect(() => {
+    app.listen(PORT);
+})
---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongodb = require("mongodb");
+const { getDb } = require("../database/mongoConnect");
+
+class Users {
+  constructor(
+    title,
+    status,
+    creator,
+    priority,
+    description,
+    date,
+    category,
+    id
+  ) {
+    this.title = title;
+    this.creator = creator;
+    this.status = status;
+    this.gender = gender;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.priority = priority;
+    this.date = date;
+    this.description = description;
+    this.category = category;
+  }
+
+  // the static key enables me call getAllUsers directly on the class itself
+  static getAllUsers() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .find()
+      .toArray()
+      .then((users) => {
+        return users;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  saveToDB() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(Id) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .find({ _id: new mongodb.ObjectId(Id) })
+      .next()
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+module.exports = Users;
