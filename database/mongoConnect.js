bb266a8781331ce2298ded3c9a4dd0f73207f913
Gerald Ezenagu <egerald344@gmail.com>
2023-11-15 08:39:32 +0000

    changes to file

--- database/mongoConnect.js
+++ database/mongoConnect.js
@@ -2,12 +2,8 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
-// "DATABASE": "node-cluster",
-//     "COLLECTION": "tickets-db",
-//     "MONGO_USER": "gerald",
-//     "MONGO_PASSWORD": "GZ3r0pV0toPBWmCV"
-// const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DATABASE}.uktzq.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`;
-const MONGODB_URL = `mongodb+srv://gerald:GZ3r0pV0toPBWmCV@node-cluster.uktzq.mongodb.net/tickets-db?retryWrites=true&w=majority`;
+ 
+const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DATABASE}.uktzq.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`;
 // const MONGODB_URL = "mongodb://localhost:27017/StudentAdmin";
 const mongoConnect = (callback) => {
   MongoClient.connect(MONGODB_URL, { useNewUrlParser: true })
